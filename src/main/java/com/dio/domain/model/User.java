34839c05ce2f3b134d9d16920b4824c9eee4a5c1
RyanRafaelA <ryrafael461@gmail.com>
2025-03-25 22:12:20 -0300

    Configuração das tabelas de entidades

--- src/main/java/com/dio/domain/model/User.java
+++ src/main/java/com/dio/domain/model/User.java
@@ -2,12 +2,27 @@ package com.dio.domain.model;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity(name = "tb_user")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	@OneToOne(cascade = CascadeType.ALL)
 	private Account account;
+	@OneToMany(cascade = CascadeType.ALL)
 	private List<Feature> features;
+	@OneToOne(cascade = CascadeType.ALL)
 	private Card card;
+	@OneToMany(cascade = CascadeType.ALL)
 	private List<News> news;
 
 	public Long getId() { return id; }
